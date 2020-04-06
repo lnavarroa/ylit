310db062785eb23a3e4f6ca8af3d448028d75d3f
root <root@localhost.localdomain>
2020-04-06 17:21:05 -0400

    instalación bootstrap

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,7 +4,14 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		$data = [
+                'title' => 'Template',
+                'welcome' => 'Bienvenido'
+        ];
+		echo view('templates/head',$data);
+		echo view('templates/header',$data);
+		echo view('templates/navbar');
+		echo view('templates/header',$data);
 	}
 
 	//--------------------------------------------------------------------
